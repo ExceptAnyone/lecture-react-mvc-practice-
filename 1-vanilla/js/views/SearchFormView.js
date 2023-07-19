89875cfd65b1394553a8c8dbd5340d074fabc77e
장정안 <wjddks96@naver.com>
2023-07-19 21:22:14 +0900

    Feat : 검색어를 입력하면 x버튼이 보이고, 검색어를 삭제하면 x버튼을 숨긴다

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,15 +1,29 @@
 import View from "./View.js";
-import { qs } from "../helpers.js";
+import { qs, on } from "../helpers.js";
+
+const tag = "[SearchFormView]";
 
 export default class SearchFormView extends View {
   constructor() {
     super(qs("#search-form-view"));
 
-    this.resetElement = qs("[type=reset]", this.element)
-    this.showResetButton(false)
+    this.inputElement = qs("[type=text]", this.element);
+    this.resetElement = qs("[type=reset]", this.element);
+    this.showResetButton(false);
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup())
+  }
+
+  handleKeyup() {
+    console.log(tag, 'handleKeyup', this.inputElement.value);
+    const {value} = this.inputElement;
+    this.showResetButton(value.length > 0)
   }
 }
