bec14bb9a210713d5b2eccda7b929256f93ba8f2
장정안 <wjddks96@naver.com>
2023-07-19 21:37:53 +0900

    Feat : 엔터를 입력하면 검색결과가 보이는 기능

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -4,8 +4,17 @@ import SearchFormView from './views/SearchFormView.js';
 export default class Controller {
   constructor(store, {SearchFormView}) {
     console.log(tag);
+
     this.store = store;
 
     this.SearchFormView = SearchFormView;
+
+    this.subscribeViewEvents();
+  }
+  subscribeViewEvents() {
+    this.SearchFormView.on('@submit', event => this.search(event.detail.value))
+  }
+  search(keyword) {
+    console.log(tag, keyword);
   }
 }
